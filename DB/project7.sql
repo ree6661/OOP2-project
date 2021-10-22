2b3257b8ea25b4b0c501c49fc5989ed9923bce30
ree6661 <45853547+ree6661@users.noreply.github.com>
2021-10-22 12:14:59 +0300

    Add files via upload

--- DB/project7.sql
+++ DB/project7.sql
@@ -1,59 +1,59 @@
 CREATE DATABASE IF NOT EXISTS deliveryDB;
-USE deliveryDB;
-CREATE TABLE IF NOT EXISTS ordrs
-	(id_orders int not null , id_categori int,fragile boolean, id_office_sender int,id_office_recipient int,
-    delivery_to_address boolean,id_customer_recipient int,id_courier int,id_status int,
-    cash_on_delivery double,payed boolean,acceptance_by_sender datetime,customer_delivery datetime,
-    PRIMARY KEY (id_orders),FOREIGN KEY (id_categori) REFERENCES categories(id_categori),
-    FOREIGN KEY (id_office_sender) REFERENCES office(id_office),FOREIGN KEY (id_office_recipient) REFERENCES office(id_office),
-    FOREIGN KEY (id_customer_recipient) REFERENCES customers(id_customer),FOREIGN KEY (id_courier) REFERENCES couriers(id_courier)
-    ,FOREIGN KEY (id_status) REFERENCES statuses(id_status));
+	USE deliveryDB;
+     CREATE TABLE IF NOT EXISTS categories
+     (id_category int not null auto_increment,categori varchar(45),
+     PRIMARY KEY (id_category));
+
     
     USE deliveryDB;
-    CREATE TABLE IF NOT EXISTS citys
-		(id_city int not null,city varchar(45),
+    CREATE TABLE IF NOT EXISTS cities
+		(id_city int not null auto_increment,city varchar(45),
         PRIMARY KEY (id_city));
     
     USE deliveryDB;
-     CREATE TABLE IF NOT EXISTS companyes
-     (id_company int not null,company varchar(45),
-     PRIMARY KEY (id_companyes));
+     CREATE TABLE IF NOT EXISTS companies
+     (id_company int not null auto_increment,company varchar(45),
+     PRIMARY KEY (id_company));
      
 	USE deliveryDB;
      CREATE TABLE IF NOT EXISTS office
-     (id_office int not null,id_company int,id_city int,
-     PRIMARY KEY (id_office),FOREIGN KEY (id_company) REFERENCES companyes(id_company),
-     FOREIGN KEY (id_city) REFERENCES citys(id_city));
+     (id_office int not null auto_increment,id_company int,id_city int,
+     PRIMARY KEY (id_office),FOREIGN KEY (id_company) REFERENCES companies(id_company),
+     FOREIGN KEY (id_city) REFERENCES cities(id_city));
      
      
 	USE deliveryDB;
      CREATE TABLE IF NOT EXISTS statuses
-     (id_status int not null,status_name varchar(45),
+     (id_status int not null auto_increment,status_name varchar(45),
      PRIMARY KEY (id_status));
      
 	USE deliveryDB;
-     CREATE TABLE IF NOT EXISTS categories
-     (id_categori int not null,categori varchar(45),
-     PRIMARY KEY (id_categori));
-     
-	USE deliveryDB;
      CREATE TABLE IF NOT EXISTS customers
-     (id_customer int not null,customer varchar(45),phone char(12),id_city int,adress varchar(45),
-     PRIMARY KEY (id_customer),FOREIGN KEY (id_city) REFERENCES citys(id_city));
+     (id_customer int not null auto_increment,customer varchar(45),phone char(12),id_city int,address varchar(45),
+     PRIMARY KEY (id_customer),FOREIGN KEY (id_city) REFERENCES cities(id_city));
      
      
 	USE deliveryDB;
      CREATE TABLE IF NOT EXISTS couriers
-     (id_courier int not null,id_office int,courier varchar(45),phone char(12),
+     (id_courier int not null auto_increment,id_office int,courier varchar(45),phone char(12),
      PRIMARY KEY (id_courier),FOREIGN KEY (id_office) REFERENCES office(id_office));
      
 	USE deliveryDB;
      CREATE TABLE IF NOT EXISTS admins
-     (id_admin int not null,categori varchar(45),phone char(12),
+     (id_admin int not null auto_increment,category varchar(45),phone char(12),
      PRIMARY KEY (id_admin));
      
 	USE deliveryDB;
      CREATE TABLE IF NOT EXISTS price_list
-     (id_company int,id_categori int, price double,
-     PRIMARY KEY (id_company , id_categori),FOREIGN KEY (id_company) REFERENCES companyes(id_company),
-    FOREIGN KEY (id_categori) REFERENCES categories(id_categori));
+     (id_company int,id_category int, price double,
+     PRIMARY KEY (id_company , id_category),FOREIGN KEY (id_company) REFERENCES companies(id_company),
+    FOREIGN KEY (id_category) REFERENCES categories(id_category));
+    
+    CREATE TABLE IF NOT EXISTS orders
+	(id_orders int not null auto_increment, id_category int,fragile boolean, id_office_sender int,id_office_recipient int,
+    delivery_to_address boolean,id_customer_recipient int,id_courier int,id_status int,
+    cash_on_delivery double,paid boolean,acceptance_by_sender datetime,customer_delivery datetime,
+    PRIMARY KEY (id_orders),FOREIGN KEY (id_category) REFERENCES categories(id_category),
+    FOREIGN KEY (id_office_sender) REFERENCES office(id_office),FOREIGN KEY (id_office_recipient) REFERENCES office(id_office),
+    FOREIGN KEY (id_customer_recipient) REFERENCES customers(id_customer),FOREIGN KEY (id_courier) REFERENCES couriers(id_courier)
+    ,FOREIGN KEY (id_status) REFERENCES statuses(id_status));
