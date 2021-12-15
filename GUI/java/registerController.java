1398f313f9cd160c32e222a143dd97b39df6add1
ree6661 <45853547+ree6661@users.noreply.github.com>
2021-12-15 19:55:32 +0200

    THE GUI
    GUIv0.1

---
+++ GUI/java/registerController.java
@@ -0,0 +1,24 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void loginFx(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
