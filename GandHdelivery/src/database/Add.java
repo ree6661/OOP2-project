fab4f258dcdba092c6526fff461aede4052e5c04
Gabriel Georgiev <44534520+GabrielGeorgievGit@users.noreply.github.com>
2021-12-17 16:51:34 +0200

    Now can add password to customers

--- GandHdelivery/src/database/Add.java
+++ GandHdelivery/src/database/Add.java
@@ -6,22 +6,22 @@ import java.sql.SQLException;
 
 public final class Add {
 	
-	public static void customer(String name, String phone, int city, String address) {
+	public static void customer(String name, String phone, int city, String address, String password) {
 		
 		try {
 			Connection conn = Create.getConnection();
 				
 			final String sql = 
 					"insert into customers(customer, phone, "
-					+ "id_city, address)"
-			        + " values (?, ?, ?, ?)";
+					+ "id_city, address, password)"
+			        + " values (?, ?, ?, ?, ?)";
 			
 			PreparedStatement st = conn.prepareStatement(sql);
 			st.setString(1, name);
 			st.setString(2, phone);
 			st.setInt(3, city);
 			st.setString(4, address);
-			//st.setString(5, password);
+			st.setString(5, password);
 			
 			st.execute();
 			st.close();
