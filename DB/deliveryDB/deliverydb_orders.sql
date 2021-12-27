b30222b962694bc230a399ed2d222599d399e7d9
Gabriel Georgiev <44534520+GabrielGeorgievGit@users.noreply.github.com>
2021-12-27 23:53:57 +0200

    Changed order of some fields in table order

--- DB/deliveryDB/deliverydb_orders.sql
+++ DB/deliveryDB/deliverydb_orders.sql
@@ -53,7 +53,7 @@ CREATE TABLE `orders` (
   CONSTRAINT `orders_ibfk_5` FOREIGN KEY (`id_customer_recipient`) REFERENCES `customers` (`id_customer`),
   CONSTRAINT `orders_ibfk_6` FOREIGN KEY (`id_courier`) REFERENCES `couriers` (`id_courier`),
   CONSTRAINT `orders_ibfk_7` FOREIGN KEY (`id_status`) REFERENCES `statuses` (`id_status`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -75,4 +75,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-12-26 22:44:44
+-- Dump completed on 2021-12-27 23:52:51
